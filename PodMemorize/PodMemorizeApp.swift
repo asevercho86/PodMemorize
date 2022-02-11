0aecc75aff51660464a7fc16287b90933c28bc91
CHO <asevercho86@gmail.com>
2022-02-11 21:21:01 +0900

    Initial Commit

---
+++ PodMemorize/PodMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodMemorizeApp.swift
+//  PodMemorize
+//
+//  Created by DEOKSHIN CHO on 2022/02/11.
+//
+
+import SwiftUI
+
+@main
+struct PodMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
